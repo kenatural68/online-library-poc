8f2fc7c7c1378f54c53426bea98d78ef8de42181
kenatural68 <86694790+kenatural68@users.noreply.github.com>
2021-12-09 17:13:17 +0800

    added profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Country {
+  name: string;
+}
+
+const COUNTRIES: Country[] = [
+  {
+    name: 'book 1',
+  },
+  {
+    name: 'book 2',
+  },
+  {
+    name: 'book 3',
+  },
+  {
+    name: 'book 4',
+  }
+];
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  countries = COUNTRIES;
 
   constructor() { }
 
